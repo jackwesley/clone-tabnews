5dc6fcde2789a7f81031645311cb06ac694e1974
Jack Wesley Moreira <jackwesley@hotmail.com>
2026-02-04 17:57:24 -0500

    Adding tests and more parameters to status endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,25 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1+1 as SUM");
-  console.log(result);
-  response.status(200).json({ message: "Yayyy all good!" });
+  const updatedAt = new Date().toISOString();
+  const databaseVersion = await database.query("SHOW server_version;");
+  const maxConnections = await database.query("SHOW max_connections;");
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseOpenedConnections = await database.query({
+    text: "SELECT COUNT(*)::int AS count FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersion.rows[0].server_version,
+        max_connections: parseInt(maxConnections.rows[0].max_connections),
+        opened_connections: databaseOpenedConnections.rows[0].count,
+      },
+    },
+  });
 }
 
 export default status;
