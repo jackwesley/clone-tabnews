568da7e8d6949328d43996921a5bb81333a6fdf0
Jack Wesley Moreira <jackwesley@hotmail.com>
2026-02-17 11:04:07 -0500

    adds api migrations endpoint

---
+++ tests/integration/api/v1/migrations/get.test.js
@@ -0,0 +1,16 @@
+import datanase from "infra/database";
+
+beforeAll(cleanDatabase);
+
+async function cleanDatabase() {
+  await datanase.query("drop schema public cascade; create schema public;");
+}
+
+test("GET to api/v1/migrations returns 200", async () => {
+  const response = await fetch("http://localhost:3000/api/v1/migrations");
+  expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  expect(Array.isArray(responseBody)).toBe(true);
+  expect(responseBody.length).toBeGreaterThan(0);
+});
